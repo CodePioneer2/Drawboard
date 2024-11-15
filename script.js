3cfa5d07dc5a7160a4fde67a87413a147b90536b
CodePioneer2 <166389859+CodePioneer2@users.noreply.github.com>
2024-11-15 11:39:05 +0200

    finish project

--- script.js
+++ script.js
@@ -1,41 +1,47 @@
-const sketch = document.querySelector('.sketch');
-const btnNewGrid = document.querySelector('.btn-new-grid');
+const area = document.querySelector('.area');
+const btnNew = document.querySelector('.btn-new');
+const btnClear = document.querySelector('.btn-clear');
+let size = 10;
+let drawing = false;
 
-function createRow(items) {
-  //making row
-  const row = document.createElement('div');
-  for (let i = 0; i < items; i++) {
-    createSquare(row);
+document.addEventListener('mousedown', () => (drawing = true));
+document.addEventListener('mouseup', () => (drawing = false));
+
+area.addEventListener('mouseover', (event) => {
+  if (drawing && event.target.classList.contains('square')) {
+    const color = `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, '0')}`;
+    event.target.style.backgroundColor = color;
   }
-  //add it to sketchboard
-  sketch.appendChild(row);
+});
+
+function createGrid(size) {
+  const squareSize = 20;
+  const width = size * squareSize;
+  area.style.width = `${width}px`;
+
+  area.innerHTML = '';
+
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    area.appendChild(square);
+  }
 }
 
-function createSquare(row) {
-  //adding more stuff to row
-  const div = document.createElement('div');
-  div.classList.add('square');
-  div.addEventListener('mouseover', (event) => {
-    event.target.style.backgroundColor = 'blue';
-    setTimeout(() => {
-      event.target.style.backgroundColor = '';
-    }, 1000);
-  });
-  row.appendChild(div);
-}
+btnNew.addEventListener('click', () => {
+  size = parseInt(prompt('ENTER GRID SIZE'), 10);
 
-function createGrid(x) {
-  sketch.innerHTML = '';
-  for (let i = 0; i < x; i++) {
-    createRow(x);
+  while (isNaN(size) || size > 100 || size <= 0) {
+    size = parseInt(prompt('Size must be a number between 1 and 100!'), 10);
   }
-}
 
-btnNewGrid.addEventListener('click', () => {
-  let size = 0;
-  size = prompt('ENTER GRID SIZE');
-  while (size > 100) {
-    size = prompt('Size needs to be smaller than 100!');
-  }
   createGrid(size);
 });
+
+btnClear.addEventListener('click', () => {
+  createGrid(size);
+});
+
+createGrid(size);
